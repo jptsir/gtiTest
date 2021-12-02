2eaf737017ae1a5ac49e98f035e7676c06f4102d
jpt <jpt@123.com>
2021-12-02 17:56:16 +0800

    second commit

--- src/main/java/atguigu/GitTest.java
+++ src/main/java/atguigu/GitTest.java
@@ -7,5 +7,8 @@ package atguigu;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git");
+        System.out.println("hello git1");
+        System.out.println("hello git2");
+        System.out.println("hello git3");
     }
 }
