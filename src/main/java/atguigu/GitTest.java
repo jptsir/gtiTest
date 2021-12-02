22618b2356be0569362f1e3820e6fac1a66dccdb
jptsir <95340992+jptsir@users.noreply.github.com>
2021-12-02 19:14:23 +0800

    push

--- src/main/java/atguigu/GitTest.java
+++ src/main/java/atguigu/GitTest.java
@@ -12,5 +12,6 @@ public class GitTest {
         System.out.println("hello git3");
         System.out.println("hot-fix");
         System.out.println("push-commit");
+        Sysout.out.println("push成功了");
     }
 }
