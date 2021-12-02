a12c3a8804910feb22a0d9a542bd6d04c4482082
jpt <jpt@123.com>
2021-12-02 17:48:39 +0800

    first commit

---
+++ src/main/java/atguigu/GitTest.java
@@ -0,0 +1,11 @@
+package atguigu;
+
+/**
+ * @author jingpengtao
+ * @create 2021-12-02 17:44
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
